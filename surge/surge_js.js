bf372f5ae370c3bd526dd4f16b0d2f62c2221b0c
onlysunjun <74503143+onlysunjun@users.noreply.github.com>
2022-05-29 16:53:37 +0800

    Update surge_js.js

--- surge/surge_js.js
+++ surge/surge_js.js
@@ -1,5 +1,5 @@
-function GetUserPoint() {
-	const pointUrl = {
+
+const pointUrl = {
 		url: 'https://api.m.jd.com',
 		headers = {
 		'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36',
@@ -11,33 +11,9 @@ function GetUserPoint() {
     			"_t": "1653682937354",
     			"appid": "market-task-h5"}
 	}
-	return new Promise((resolve) => { 
-		$.post(pointUrl, (error, resp, data) => { 
-			try { 
-				if (error) {
-					throw new Error(error); 
-				} else {
-					const body = JSON.parse(data); 
-					if (body.code == 0 && body.data) { 
-						user.point = parseInt(body.data.gears; \
-						console.log(`\n可换红包: ${body.data.gears}`); //打印日志
-					} else { //否则抛出一个异常
-						throw new Error(body.msg || data);
-					}
-				}
-			} catch (e) { //接住try代码块中抛出的异常, 并打印日志
-				console.log(`\n查询红包: 失败\n出现错误: ${e.message}`);
-			} finally { //finally语句在try和catch之后无论有无异常都会执行
-				resolve(); //异步操作成功时调用, 将Promise对象的状态标记为"成功", 表示已完成查询积分
-			}
-		})
-	})
-}
 
-
-(async function() { 
-	await Promise.all([ 
-		GetUserPoint()
-	]);
-	$.done(); 
-})()
+$httpclient.post(pointUrl, (error, resp, data){
+	const body = JSON.parse(data)
+	console.log(`\n可换红包: ${body.data.gears}`)
+$.done()}
+		
